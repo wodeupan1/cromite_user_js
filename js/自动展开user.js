932610a46d55c542e0233b878e0350e1c023694e
wodeupan1 <wodeupan1@163.com>
2023-09-20 12:27:46 +0000

    updated logs

--- js/自动展开user.js
+++ js/自动展开user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动展开
-// @version      1.3.60
+// @version      1.3.61
 // @namespace    https://github.com/AirBashX/AutoUnfold/
 // @homepageURL  https://github.com/AirBashX/UserScript
 // @author       airbash
@@ -922,21 +922,9 @@
 						item.click();
 					}
 
-					//展开(PC端:评论)
-					let items3 = document.querySelectorAll(".expand");
+					//展开(PC端:评论;简介;人物简介)
+					let items3 = document.querySelectorAll(".expand,.a_show_full,.fold-switch");
 					for (let item of items3) {
-						item.click();
-					}
-
-					//展开全部(PC端:简介)
-					let items4 = document.querySelectorAll(".a_show_full");
-					for (let item of items4) {
-						item.click();
-					}
-
-					//展开(PC端:人物简介)
-					let items5 = document.querySelectorAll(".fold-switch");
-					for (let item of items5) {
 						item.click();
 					}
 				};
